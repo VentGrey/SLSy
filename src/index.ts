dd3f889c24eda85d90281c8578b96d314b4ba58e
VentGrey <omarpurataf@gmail.com>
2024-05-12 20:35:36 -0600

    Add base code

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+
+/**
+ * Tiny Oak security middleware similar to Express.js's helmet
+ * heavily inspired on [snelm](https://deno.land/x/snelm@1.3.0)
+ * 
+ * 
+ */
+export class slsy {
+  private _options: any;
+}
