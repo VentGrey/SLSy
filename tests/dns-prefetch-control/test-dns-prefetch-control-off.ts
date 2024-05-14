c23d7dc7fa47d1ac0d8e62a24faee7ab07ecd466
VentGrey <omarpurataf@gmail.com>
2024-05-14 17:09:23 -0600

    Add dns-prefetch-control tests

---
+++ tests/dns-prefetch-control/test-dns-prefetch-control-off.ts
@@ -0,0 +1,37 @@
+import { testing } from "@oak/oak";
+import type { Context, Middleware, Next } from "@oak/oak";
+import { assertEquals } from "@std/assert";
+
+import { Slsy } from "../../src/index.ts";
+
+const slsy: Slsy = new Slsy({
+    expectCt: null,
+    crossdomain: null,
+    dnsPrefetchControl: {
+        allow: false,
+    }
+});
+
+const mw: Middleware = async (ctx: Context, next: Next) => {
+    ctx.response = slsy.slsy(ctx.request, ctx.response);
+    await next();
+};
+
+Deno.test({
+    name: "Test DNS Prefetch Control header -  Value: off",
+    ignore: Deno.build.os === "windows",
+    fn: async () => {
+        const ctx = testing.createMockContext();
+        const next = testing.createMockNext();
+
+        await mw(ctx, next);
+
+        // Test for "off" as "dns-prefetch-control" header value
+        assertEquals(
+            ctx.response.headers.get("x-dns-prefetch-control"),
+            "off",
+        );
+    },
+});
+
+
---
+++ tests/dns-prefetch-control/test-dns-prefetch-control-on.ts
@@ -0,0 +1,36 @@
+import { testing } from "@oak/oak";
+import type { Context, Middleware, Next } from "@oak/oak";
+import { assertEquals } from "@std/assert";
+
+import { Slsy } from "../../src/index.ts";
+
+const slsy: Slsy = new Slsy({
+    expectCt: null,
+    crossdomain: null,
+    dnsPrefetchControl: {
+        allow: true,
+    }
+});
+
+const mw: Middleware = async (ctx: Context, next: Next) => {
+    ctx.response = slsy.slsy(ctx.request, ctx.response);
+    await next();
+};
+
+Deno.test({
+    name: "Test DNS Prefetch Control header -  Value: on",
+    ignore: Deno.build.os === "windows",
+    fn: async () => {
+        const ctx = testing.createMockContext();
+        const next = testing.createMockNext();
+
+        await mw(ctx, next);
+
+        // Test for "on" as "dns-prefetch-control" header value
+        assertEquals(
+            ctx.response.headers.get("x-dns-prefetch-control"),
+            "on",
+        );
+    },
+});
+
